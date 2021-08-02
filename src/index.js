126ca3dd8610bfecde740860535baf60f24c1911
SimonGrchevski <simongrcevski@gmail.com>
2021-08-02 21:12:45 +0200

    Add the header with basic styles

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+import logo from './assets/R&M-logo.png';
+
+
+const logoWrapper = document.querySelector('.logo');
+const img = document.createElement('img');
+img.src = logo;
+logoWrapper.append(img);
