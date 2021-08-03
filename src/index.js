444052d4391a3a46f67ed4746071a735bdea53f6
moise10r <moiserushanika2006@gmail.com>
2021-08-03 19:41:12 +0200

    Fix js errors.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import logo from './assets/R&M-logo.png';
 
-
 const logoWrapper = document.querySelector('.logo');
 const img = document.createElement('img');
 img.src = logo;
