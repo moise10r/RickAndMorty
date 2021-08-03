e6976f8c9293dd7660633db9902b97bfc580218f
SimonGrchevski <simongrcevski@gmail.com>
2021-08-03 20:35:24 +0200

    Add eventListeners to the comment buttons

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 import './style.css';
 import logo from './assets/R&M-logo.png';
-import {
-  getCharacters,
-  postComment,
-  getComment,
-  postLike,
-} from './controllers';
+import { getCharacters } from './controllers';
 
 const logoWrapper = document.querySelector('.logo');
 const img = document.createElement('img');
 img.src = logo;
 logoWrapper.append(img);
 
-async function test() {
-  const like = await postLike({
-    item_id: 'Moise',
+let items = [];
+const render = async() => {
+  items = await getCharacters();
+  const list = document.querySelector('.items-list');
+  list.innerHTML = '';
+  items.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.innerHTML = `  <div class="img-wrapper">
+    <div class="img-bg"></div>
+    <img src='${item.image}' alt="Img">
+  </div>
+    <div class="like-content flex">
+      <div class="title">${item.name}</div>
+      <div class="like flex">
+      <span><i class="fas fa-heart"></i></span>
+      <span>
+          <span>Like</span>
+          <span>2</span>
+        </span>
+      </div>
+    </div>
+    <div class="comment"><button id='${item.id}' class="btn">Comment</button></div>
+    <div class="reserv"><button class="btn">Reservation</button></div>`;
+    list.appendChild(li);
   });
-  const postCom = await postComment({
-    item_id: 'item1',
-    username: 'Jane5',
-    comment: 'Hello5',
-  });
-  const getCharact = await getCharacters();
-  const getCom = await getComment();
-  console.log({
-    like,
-    postCom,
-    getCharact,
-    getCom,
-  });
-}
-test();
+  [...document.querySelectorAll('.btn')].forEach(elem => {
+    elem.addEventListener('click', () => {
+      console.log(items[elem.id - 1]);
+    })
+  })
+};
+
+render();
