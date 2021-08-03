d4f2a1ce6cd15774d8b66af6442b609b203c64af
moise10r <moiserushanika2006@gmail.com>
2021-08-03 18:33:29 +0200

    Remove unnecessary code.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import './style.css';
 import logo from './assets/R&M-logo.png';
-import imgItem from './assets/Portfolio_2.png';
-import {
-  getCharacters, postComment, getComment, postLike,
-} from './controllers';
+import { getCharacters } from './controllers';
 
 const logoWrapper = document.querySelector('.logo');
 const img = document.createElement('img');
 img.src = logo;
 logoWrapper.append(img);
-
-// async function render() {
-//   const like = await postLike({
-//     item_id: 'Moise',
-//   });
-//   const postCom = await postComment({
-//     item_id: 'item1',
-//     username: 'Jane5',
-//     comment: 'Hello5',
-//   });
-//   const getCharact = await getCharacters();
-//   const getCom = await getComment();
-//   console.log({
-//     like,
-//     postCom,
-//     getCharact,
-//     getCom,
-//   });
-// }
-// render();
-// const items = [...getCharacters];
-// console.log(items);
-
-
 const render = async () => {
   const items = await getCharacters();
   const list = document.querySelector('.items-list');
