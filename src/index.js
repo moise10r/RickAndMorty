9d679b00ae7b154f291540565e9f1e95e4ca369c
moise10r <moiserushanika2006@gmail.com>
2021-08-03 18:30:28 +0200

    Add item dynamically form Api.

--- src/index.js
+++ src/index.js
@@ -10,129 +10,53 @@ const img = document.createElement('img');
 img.src = logo;
 logoWrapper.append(img);
 
-async function render() {
-  const like = await postLike({
-    item_id: 'Moise',
+// async function render() {
+//   const like = await postLike({
+//     item_id: 'Moise',
+//   });
+//   const postCom = await postComment({
+//     item_id: 'item1',
+//     username: 'Jane5',
+//     comment: 'Hello5',
+//   });
+//   const getCharact = await getCharacters();
+//   const getCom = await getComment();
+//   console.log({
+//     like,
+//     postCom,
+//     getCharact,
+//     getCom,
+//   });
+// }
+// render();
+// const items = [...getCharacters];
+// console.log(items);
+
+
+const render = async () => {
+  const items = await getCharacters();
+  const list = document.querySelector('.items-list');
+  list.innerHTML = '';
+  items.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.innerHTML = `  <div class="img-wrapper">
+    <img src='${item.image}' alt="Img">
+  </div>
+    <div class="like-content flex">
+      <div class="title">${item.name}</div>
+      <div class="like flex">
+      <span><i class="fas fa-heart"></i></span>
+      <span>
+          <span>Like</span>
+          <span>2</span>
+        </span>
+      </div>
+    </div>
+    <div class="comment"><button class="btn">Comment</button></div>
+    <div class="reserv"><button class="btn">Reservation</button></div>`;
+    list.appendChild(li);
   });
-  const postCom = await postComment({
-    item_id: 'item1',
-    username: 'Jane5',
-    comment: 'Hello5',
-  });
-  const getCharact = await getCharacters();
-  const getCom = await getComment();
-  console.log({
-    like,
-    postCom,
-    getCharact,
-    getCom,
-  });
-}
+};
+
 render();
-
-const list = document.querySelector('.items-list');
-
-list.innerHTML = `
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-<li class="item">
-<div class="img-wrapper">
-  <img src='${imgItem}' alt="Img">
-</div>
-  <div class="like-content flex">
-    <div class="title">Title</div>
-    <div class="like flex">
-    <span><i class="fas fa-heart"></i></span>
-    <span>
-        <span>Like</span>
-        <span>2</span>
-      </span>
-    </div>
-  </div>
-  <div class="comment"><button class="btn">Comment</button></div>
-  <div class="reserv"><button class="btn">Reservation</button></div>
-</li>
-`;
