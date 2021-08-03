6c24e8fe6eaa005aa6786a09907bbde6cba0de94
moise10r <moiserushanika2006@gmail.com>
2021-08-03 18:41:39 +0200

    Fix eslint errors.

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
   module: {
     rules: [
       {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
