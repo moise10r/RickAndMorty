7533a468f2b82b503d70cd9d483af620e6f317a7
SimonGrchevski <simongrcevski@gmail.com>
2021-08-04 15:30:49 +0200

    Fix linter Errors

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,70 @@
 /* eslint-disable import/prefer-default-export */
 import { getComment, postComment } from './controllers';
 
-
 const createElem = (tag, cl) => {
   const elem = document.createElement(tag);
   elem.classList.add(...cl);
   return elem;
 };
 
-const wrapper = createElem('section', ['popup', 'flex', 'flex-col']); // popup wrapper 
+const wrapper = createElem('section', ['popup', 'flex', 'flex-col']); // popup wrapper
 let comments = []; // comments array
+
+const updateCommentCounts = () => {
+  wrapper.querySelector('.comments-count').innerHTML = comments.length;
+};
+
+const removePopup = () => {
+  const main = document.querySelector('main');
+  main.removeChild(main.lastChild);
+};
+
+const setCloseEvent = (element) => {
+  element.querySelector('.close').addEventListener('click', () => {
+    removePopup();
+  });
+};
+
+const createDate = () => {
+  const d = new Date();
+  const day = (`0${d.getDate()}`).slice(-2);
+  const month = (`0${d.getMonth() + 1}`).slice(-2);
+  const year = d.getFullYear();
+
+  return `${year}-${month}-${day}`;
+};
+
+const updateComments = () => {
+  const ul = wrapper.querySelector('.comments-ul');
+  const li = document.createElement('li');
+  const comment = comments[comments.length - 1];
+  const date = createDate();
+  li.innerHTML = `<span class='comment-date'>${date}</span>
+    <span class='comment-user'>${comment.username}: </span>
+    <span class='comment-comment'>${comment.comment}</span>`;
+  ul.append(li);
+};
+
+const addComment = async (content, id) => {
+  const nameInp = wrapper.querySelector('.name-inp');
+  const commentInp = wrapper.querySelector('.comment-inp');
+  const name = nameInp.value;
+  const comment = commentInp.value;
+  if (name && comment) {
+    comments.push({ item_id: id, username: name, comment });
+    updateComments();
+    updateCommentCounts();
+    nameInp.value = '';
+    commentInp.value = '';
+    postComment({ item_id: id, username: name, comment });
+  }
+};
+
+const setAddComentEvent = (content, id) => {
+  wrapper.querySelector('.add-comment-btn').addEventListener('click', () => {
+    addComment(content, id);
+  });
+};
 
 const createImageWrapper = (img) => {
   const imageWrapper = createElem('article', ['image-wrapper', 'flex', 'flex-row']);
@@ -42,8 +97,7 @@ const createCharInfoWrapper = (info) => {
 
 const createCommentsWrapper = async (id) => {
   comments = await getComment(id);
-  if (!Array.isArray(comments))
-    comments = [];
+  if (!Array.isArray(comments)) comments = [];
   const elem = createElem('article', ['flex', 'flex-col', 'comment-wrapper']);
   elem.innerHTML = `<div class='text-center padding-20'>
         <h3>Comments (<span class='comments-count'>${comments.length}</span>)</h3>
@@ -60,7 +114,6 @@ const createCommentsWrapper = async (id) => {
   elem.append(ul);
   return elem;
 };
-
 
 const createAddCommentSection = () => {
   const wrap = createElem('section', ['add-comment-section', 'flex', 'flex-col']);
@@ -82,7 +135,7 @@ const createAddCommentSection = () => {
           <input type="button" value='Comment' class='add-comment-btn btn'>
         </article>`;
   return wrap;
-}
+};
 
 export const createPopup = async (content, id) => {
   wrapper.innerHTML = '';
@@ -90,7 +143,7 @@ export const createPopup = async (content, id) => {
     createImageWrapper(content.image),
     createCharInfoWrapper(content),
     await createCommentsWrapper(id),
-    createAddCommentSection()
+    createAddCommentSection(),
   );
 
   setCloseEvent(wrapper);
@@ -98,59 +151,3 @@ export const createPopup = async (content, id) => {
 
   document.querySelector('main').append(wrapper);
 };
-
-const setCloseEvent = (element) => {
-  element.querySelector('.close').addEventListener('click', () => {
-    removePopup();
-  });
-}
-
-const setAddComentEvent = (content, id) => {
-  wrapper.querySelector('.add-comment-btn').addEventListener('click', () => {
-    addComment(content, id);
-  });
-}
-
-const removePopup = () => {
-  const main = document.querySelector('main');
-  main.removeChild(main.lastChild);
-}
-
-const createDate = () => {
-  const d = new Date();
-  const day = ('0' + d.getDate()).slice(-2);
-  const month = ('0' + (d.getMonth() + 1)).slice(-2);
-  const year = d.getFullYear();
-
-  return `${year}-${month}-${day}`;
-}
-
-const updateComments = () => {
-  const ul = wrapper.querySelector('.comments-ul');
-  const li = document.createElement('li');
-  const comment = comments[comments.length - 1];
-  const date = createDate();
-  li.innerHTML = `<span class='comment-date'>${date}</span>
-    <span class='comment-user'>${comment.username}: </span>
-    <span class='comment-comment'>${comment.comment}</span>`;
-  ul.append(li);
-}
-
-const updateCommentCounts = () => {
-  wrapper.querySelector('.comments-count').innerHTML = comments.length;
-}
-
-const addComment = async (content, id) => {
-  const nameInp = wrapper.querySelector('.name-inp')
-  const commentInp = wrapper.querySelector('.comment-inp');
-  const name = nameInp.value;
-  const comment = commentInp.value;
-  if (name && comment) {
-    comments.push({ 'item_id': id, 'username': name, comment: comment });
-    updateComments();
-    updateCommentCounts();
-    nameInp.value = '';
-    commentInp.value = '';
-    postComment({ 'item_id': id, 'username': name, comment: comment });
-  }
-}
